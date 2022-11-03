8dc0503fb0eee49c444aa3ba19c510ad1f2d8c69
dessinger <juliancorrea1403@gmail.com>
2022-11-03 17:31:02 -0300

    agregando node.js , sass, y gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,34 @@
-function tarea(done){
-    console.log('mi primer tarea');
+// function tarea(done){
+//     console.log('mi primer tarea');
 
-    done()
+//     done()
+// }
+
+// exports.tarea = tarea;
+
+
+
+const  {src, dest, watch }  = require ("gulp");
+const sass = require('gulp-sass')(require('sass'));
+
+
+function css (done){
+
+    src("src/scss/app.scss") //indentificar el archivo SASS
+    .pipe( sass())  //Compilarlo
+     .pipe(dest("build/css"));//Almacenar en el disco duro
+
+done() ;   
 }
 
-exports.primerTarea = tarea;
+function dev (done){
+    watch("src/scss/app.scss", css);
+
+    
+    done();
+}
+
+exports.css = css;
+exports.dev = dev;
+
+
